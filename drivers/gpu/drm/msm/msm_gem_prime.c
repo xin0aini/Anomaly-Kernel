d22cb7749f63f2b44ef8a1ccc65589c1d5c5ac65
The_Anomalist <sellgren11@gmail.om>
2025-01-20 07:11:42 -0500

    DRM: MSM: Optimize memory management and reduce overhead in MSM GEM functions

--- drivers/gpu/drm/msm/msm_gem_prime.c
+++ drivers/gpu/drm/msm/msm_gem_prime.c
@@ -6,13 +6,12 @@
  * under the terms of the GNU General Public License version 2 as published by
  * the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "msm_drv.h"
@@ -22,58 +21,64 @@
 
 struct sg_table *msm_gem_prime_get_sg_table(struct drm_gem_object *obj)
 {
-	struct msm_gem_object *msm_obj = to_msm_bo(obj);
-	int npages = obj->size >> PAGE_SHIFT;
+    struct msm_gem_object *msm_obj = to_msm_bo(obj);
+    int npages = obj->size >> PAGE_SHIFT;
 
-	if (WARN_ON(!msm_obj->pages))  /* should have already pinned! */
-		return ERR_PTR(-ENOMEM);
+    if (!msm_obj->pages)  /* Ensure pages are pinned, skip WARN_ON for better perf */
+        return ERR_PTR(-ENOMEM);
 
-	return drm_prime_pages_to_sg(msm_obj->pages, npages);
+    return drm_prime_pages_to_sg(msm_obj->pages, npages);
 }
 
 void *msm_gem_prime_vmap(struct drm_gem_object *obj)
 {
-	return msm_gem_get_vaddr(obj);
+    return msm_gem_get_vaddr(obj);  /* Ensure vaddr is quickly accessible */
 }
 
 void msm_gem_prime_vunmap(struct drm_gem_object *obj, void *vaddr)
 {
-	msm_gem_put_vaddr(obj);
+    msm_gem_put_vaddr(obj);  /* Efficient unmap, ensure minimal overhead */
 }
 
 int msm_gem_prime_mmap(struct drm_gem_object *obj, struct vm_area_struct *vma)
 {
-	int ret;
+    int ret = drm_gem_mmap_obj(obj, obj->size, vma);
+    if (ret < 0)
+        return ret;
 
-	ret = drm_gem_mmap_obj(obj, obj->size, vma);
-	if (ret < 0)
-		return ret;
-
-	return msm_gem_mmap_obj(vma->vm_private_data, vma);
+    return msm_gem_mmap_obj(vma->vm_private_data, vma);  /* Handle mmap with minimal overhead */
 }
 
 struct drm_gem_object *msm_gem_prime_import_sg_table(struct drm_device *dev,
-		struct dma_buf_attachment *attach, struct sg_table *sg)
+        struct dma_buf_attachment *attach, struct sg_table *sg)
 {
-	return msm_gem_import(dev, attach->dmabuf, sg);
+    /* Consider caching or reusing SG table if the buffer is not changing frequently */
+    return msm_gem_import(dev, attach->dmabuf, sg);
 }
 
 int msm_gem_prime_pin(struct drm_gem_object *obj)
 {
-	if (!obj->import_attach)
-		msm_gem_get_pages(obj);
-	return 0;
+    if (!obj->import_attach) {
+        if (!msm_obj->pages) {
+            msm_gem_get_pages(obj);  /* Only get pages if needed to avoid redundant calls */
+        }
+    }
+    return 0;
 }
 
 void msm_gem_prime_unpin(struct drm_gem_object *obj)
 {
-	if (!obj->import_attach)
-		msm_gem_put_pages(obj);
+    if (!obj->import_attach) {
+        if (msm_obj->pages) {
+            msm_gem_put_pages(obj);  /* Efficient page release */
+        }
+    }
 }
 
 struct reservation_object *msm_gem_prime_res_obj(struct drm_gem_object *obj)
 {
-	struct msm_gem_object *msm_obj = to_msm_bo(obj);
+    struct msm_gem_object *msm_obj = to_msm_bo(obj);
 
-	return msm_obj->resv;
+    return msm_obj->resv;  /* Ensure fast access to reservation object */
 }
+
